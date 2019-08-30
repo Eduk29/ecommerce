c01590cc23c1346b73f4b845311d59b6f1a84c88
Eduardo Marques <edu.temarques@gmail.com>
2019-08-29 21:11:24 -0300

    feat: Add Router in Application

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,17 @@
+// React
 import React from "react"
-import logo from "../assets/logo.svg"
-import "./App.css"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+// Pages
+import Home from "../pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="" component={Home} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
