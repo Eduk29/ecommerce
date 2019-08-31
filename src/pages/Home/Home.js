75d27448cc1913d96da05975aae5856b43811638
Eduardo Marques <edu.temarques@gmail.com>
2019-08-31 18:33:03 -0300

    feat: Adjusts in Home Page (SCSS)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,14 @@ import React from "react"
 
 // Style
 import "../../style/pages/Home.scss"
+import Navbar from "../../components/Navbar"
 
 const Home = () => {
   return (
     <div className="ecommerce__home">
       <div className="ecommerce__home--header">
         <div className="ecommerce__home--header--navbar">
-          <h4>Navbar</h4>
+          <Navbar />
         </div>
       </div>
       <div className="ecommerce__home--main">
