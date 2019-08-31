c2ebcb378b290a8ecdcaa0f01b0cf3e6168715ee
Eduardo Marques <edu.temarques@gmail.com>
2019-08-31 14:12:29 -0300

    feat: Create Home Page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+// React
+import React from "react"
+
+// Style
+import "../../style/pages/Home.scss"
+
+const Home = () => {
+  return (
+    <div className="ecommerce__home">
+      <div className="ecommerce__home--header">
+        <div className="ecommerce__home--header--navbar">
+          <h4>Navbar</h4>
+        </div>
+      </div>
+      <div className="ecommerce__home--main">
+        <h1>Main</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
