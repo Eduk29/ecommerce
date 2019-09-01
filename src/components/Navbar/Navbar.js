59dffe5cf7ccaf935aa5638c892a320c914c9dca
Eduardo Marques <edu.temarques@gmail.com>
2019-09-01 14:15:12 -0300

    feat: Add fontAwesome

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,21 @@
 // React
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
+
+import "../../style/components/Navbar.scss"
 
 const Navbar = () => {
-  return <div>Isto é uma navbar</div>
+  return (
+    <div className="ecommerce__navbar">
+      <div className="ecommerce__navbar--brand">E-Commerce</div>
+      <div className="ecommerce__navbar--cart">
+        <button type="button">
+          <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
