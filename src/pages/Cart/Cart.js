7f3c5b4bc5b9d5f9d10f3358cd22061b39052c74
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-09-02 16:40:54 -0300

    feat: Impl. Cart Page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,10 +1,18 @@
 // React
 import React from "react"
 
+// Components
+import Navbar from "../../components/Navbar"
+
 const Cart = () => {
   return (
-    <div>
-      <h1> Page Cart </h1>
+    <div className="ecommerce__cart">
+      <div className="ecommerce__cart-navbar">
+        <Navbar />
+      </div>
+      <div className="ecommerce__cart-main">
+        <h1> Page Cart </h1>
+      </div>
     </div>
   )
 }
