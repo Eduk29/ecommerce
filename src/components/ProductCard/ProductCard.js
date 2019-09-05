0c1cf4b7e45976be67854dcdf5f66d931085c07b
Eduardo Marques <edu.temarques@gmail.com>
2019-09-04 21:46:15 -0300

    feat: Text styles

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -5,9 +5,25 @@ import React from "react"
 import "../../style/components/ProductCard.scss"
 
 const ProductCard = () => {
+  const name = "Tênis Adidas"
+  const description =
+    "O melhor e mais moderno tênis da atualidade. Seu design em couro garante um bem estar durante o seu uso."
+  const price = "R$ 12.00"
+
   return (
     <div className="ecommerce__product-card">
-      <div className="ecommerce__product-card-image">Image</div>
+      <img
+        src="https://imgcentauro-a.akamaihd.net/900x900/93008651/tenis-adidas-baseline-k-infantil-img.jpg"
+        alt="product"
+        className="ecommerce__product-card-image"
+      />
+      <div className="ecommerce__product-card-data">
+        <div className="ecommerce__product-card-data-name">{name}</div>
+        <div className="ecommerce__product-card-data-description">
+          {description}
+        </div>
+        <div className="ecommerce__product-card-data-price">{price}</div>
+      </div>
     </div>
   )
 }
