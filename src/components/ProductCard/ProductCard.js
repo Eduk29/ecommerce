5488fb154b5a3a115c756c08d76a01b71e7df0a9
Eduardo Marques <edu.temarques@gmail.com>
2019-09-07 22:03:15 -0300

    feat: Add button to product card

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,11 +1,18 @@
 // React
 import React from "react"
 
+// Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+
 // Styles
 import "../../style/components/ProductCard.scss"
+import Button from "../Button/Button"
 
 const ProductCard = () => {
   const name = "Tênis Adidas"
+  const imageUrl =
+    "https://imgcentauro-a.akamaihd.net/900x900/93008651/tenis-adidas-baseline-k-infantil-img.jpg"
   const description =
     "O melhor e mais moderno tênis da atualidade. Seu design em couro garante um bem estar durante o seu uso."
   const price = "R$ 12.00"
@@ -13,7 +20,7 @@ const ProductCard = () => {
   return (
     <div className="ecommerce__product-card">
       <img
-        src="https://imgcentauro-a.akamaihd.net/900x900/93008651/tenis-adidas-baseline-k-infantil-img.jpg"
+        src={imageUrl}
         alt="product"
         className="ecommerce__product-card-image"
       />
@@ -23,6 +30,15 @@ const ProductCard = () => {
           {description}
         </div>
         <div className="ecommerce__product-card-data-price">{price}</div>
+      </div>
+      <div className="ecommerce__product-card-footer">
+        <Button
+          label="Add to Cart"
+          icon={<FontAwesomeIcon icon={faPlus} size="lg" />}
+          iconPosition="left"
+          borderRadius={25}
+          color="default"
+        />
       </div>
     </div>
   )
