849fc91793ac149c1a23226629f030727051046a
Eduardo Marques <edu.temarques@gmail.com>
2019-09-11 22:31:58 -0300

    feat: Add ItemList Container Logics, Product Service Integration  and SCSS

--- src/containers/ItemList/ItemList.js
+++ src/containers/ItemList/ItemList.js
@@ -1,9 +1,35 @@
-import React from "react"
+// React
+import React, { useState, useEffect } from "react"
+
+// Components
+import ProductCard from "../../components/ProductCard"
+
+// Services
+import * as ProductService from "../../services/Products/ProductService"
+
+// Styles
+import "../../style/containers/ItemList.scss"
 
 const ItemList = () => {
+  const [products, setProducts] = useState([])
+
+  const getAllProducts = () => {
+    ProductService.getAll().then(response => setProducts(response))
+  }
+
+  useEffect(() => {
+    getAllProducts()
+  })
+
   return (
-    <div>
-      <h1>Containe Item List</h1>
+    <div className="ecommerce__item-list-container">
+      {products.map(item => {
+        return (
+          <div className="ecommerce_item-list--item">
+            <ProductCard key={item.id} />
+          </div>
+        )
+      })}
     </div>
   )
 }
