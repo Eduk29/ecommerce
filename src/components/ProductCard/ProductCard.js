dccece4bc794146dade7cf4457c8b100cc662c17
Eduardo Marques <edu.temarques@gmail.com>
2019-09-14 15:05:13 -0300

    feat: Integration with Product Service and dispaly logics

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -9,32 +9,27 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons"
 import "../../style/components/ProductCard.scss"
 import Button from "../Button/Button"
 
-const ProductCard = () => {
-  const name = "Tênis Adidas"
-  const imageUrl =
-    "https://imgcentauro-a.akamaihd.net/900x900/93008651/tenis-adidas-baseline-k-infantil-img.jpg"
-  const description =
-    "O melhor e mais moderno tênis da atualidade. Seu design em couro garante um bem estar durante o seu uso."
-  const price = "R$ 12.00"
-
+const ProductCard = ({ product }) => {
   return (
     <div className="ecommerce__product-card">
       <img
-        src={imageUrl}
+        src={product.urlImage}
         alt="product"
         className="ecommerce__product-card-image"
       />
       <div className="ecommerce__product-card-data">
-        <div className="ecommerce__product-card-data-name">{name}</div>
+        <div className="ecommerce__product-card-data-name">{product.name}</div>
         <div className="ecommerce__product-card-data-description">
-          {description}
+          {product.description}
         </div>
-        <div className="ecommerce__product-card-data-price">{price}</div>
+        <div className="ecommerce__product-card-data-price">
+          R$ {product.price}
+        </div>
       </div>
       <div className="ecommerce__product-card-footer">
         <Button
           label="Add to Cart"
-          icon={<FontAwesomeIcon icon={faPlus} size="lg" />}
+          icon={<FontAwesomeIcon icon={faPlus} size="sm" />}
           iconPosition="left"
           borderRadius={25}
           color="default"
--- src/containers/ItemList/ItemList.js
+++ src/containers/ItemList/ItemList.js
@@ -10,6 +10,15 @@ import * as ProductService from "../../services/Products/ProductService"
 // Styles
 import "../../style/containers/ItemList.scss"
 
+// const product = {
+//   name: "Tênis Adidas",
+//   imageUrl:
+//     "https://imgcentauro-a.akamaihd.net/900x900/93008651/tenis-adidas-baseline-k-infantil-img.jpg",
+//   description:
+//     "O melhor e mais moderno tênis da atualidade. Seu design em couro garante um bem estar durante o seu uso.",
+//   price: "R$ 22.00",
+// }
+
 const ItemList = () => {
   const [products, setProducts] = useState([])
 
@@ -19,14 +28,14 @@ const ItemList = () => {
 
   useEffect(() => {
     getAllProducts()
-  })
+  }, [])
 
   return (
     <div className="ecommerce__item-list-container">
-      {products.map(item => {
+      {products.map(product => {
         return (
-          <div className="ecommerce_item-list--item">
-            <ProductCard key={item.id} />
+          <div key={product.id} className="ecommerce_item-list--item">
+            <ProductCard product={product} />
           </div>
         )
       })}
