79f2d7d39e5bb5e46e2c531ad5e55dece4c2d5c6
Eduardo Marques <edu.temarques@gmail.com>
2019-09-23 22:30:13 -0300

    feat (WIP): Add optionList prop

--- src/components/InputSelect/InputSelect.js
+++ src/components/InputSelect/InputSelect.js
@@ -4,17 +4,28 @@ import React from "react"
 // Styles
 import "../../style/components/InputSelect.scss"
 
-const InputSelect = () => {
+const InputSelect = props => {
+  const { optionsList } = props
+
   const handleSelect = event => {
     console.log("Event: ", event.target.value)
   }
 
   return (
-    <select onChange={handleSelect} className="ecommerce__input-select">
-      <option value="option1">Option 1</option>
-      <option value="option2">Option 2</option>
-      <option value="option3">Option 3</option>
-      <option value="option4">Option 4</option>
+    <select
+      onChange={handleSelect}
+      className="ecommerce__input-select"
+      placeholder="Teste"
+    >
+      <option value="" hidden>
+        Choose one
+      </option>
+
+      {optionsList.map(option => (
+        <option key={option.id} value={option.value}>
+          {option.key}
+        </option>
+      ))}
     </select>
   )
 }
--- src/pages/TestComponents/TestComponents.js
+++ src/pages/TestComponents/TestComponents.js
@@ -1,10 +1,28 @@
 import React from "react"
 import InputSelect from "../../components/InputSelect/InputSelect"
 
+const optionsList = [
+  {
+    id: 1,
+    key: "Option 1",
+    value: "option1",
+  },
+  {
+    id: 2,
+    key: "Option 2",
+    value: "option2",
+  },
+  {
+    id: 3,
+    key: "Option 3",
+    value: "option3",
+  },
+]
+
 const TestComponents = () => {
   return (
     <div>
-      <InputSelect />
+      <InputSelect optionsList={optionsList} />
     </div>
   )
 }
