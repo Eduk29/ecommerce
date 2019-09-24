e854443cec79db688bd868763eb5d43cb2d2eafa
Eduardo Marques <edu.temarques@gmail.com>
2019-09-23 22:07:49 -0300

    feat: Add components test page

---
+++ src/pages/TestComponents/TestComponents.js
@@ -0,0 +1,12 @@
+import React from "react"
+import InputSelect from "../../components/InputSelect/InputSelect"
+
+const TestComponents = () => {
+  return (
+    <div>
+      <InputSelect />
+    </div>
+  )
+}
+
+export default TestComponents
