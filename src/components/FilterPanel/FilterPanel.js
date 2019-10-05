6b49e458c5d6bb9c72683c0693957a64e258b357
Eduardo Marques <edu.temarques@gmail.com>
2019-10-05 20:35:11 -0300

    feat (WIP): Impl. Filter Panel Component

--- src/components/FilterPanel/FilterPanel.js
+++ src/components/FilterPanel/FilterPanel.js
@@ -1,7 +1,60 @@
 import React from "react"
+import InputSelect from "../InputSelect/InputSelect"
 
-const FilterPanel = () => {
-  return <div>default Filter Panel component</div>
+import "../../style/components/FilterPanel.scss"
+import Button from "../Button"
+
+const FilterPanel = ({
+  brands,
+  categories,
+  handleChangeBrandFilter,
+  handleChangeCategoryFilter,
+  handleClickFilter,
+  selectedBrand,
+  selectedCategory,
+}) => {
+  const displayBrandsFilter = () => {
+    return brands && brands.length > 0
+  }
+
+  const displayCategoriesFilter = () => {
+    return categories && categories.length > 0
+  }
+
+  return (
+    <div className="ecommerce__filter-panel">
+      <div className="ecommerce__filter-panel-brands">
+        {displayBrandsFilter() && (
+          <InputSelect
+            handleSelectOption={handleChangeBrandFilter}
+            label="Brands: "
+            optionsList={brands}
+            value={selectedBrand.value}
+          />
+        )}
+      </div>
+      <div className="ecommerce__filter-panel-category">
+        {displayCategoriesFilter() && (
+          <InputSelect
+            handleSelectOption={handleChangeCategoryFilter}
+            label="Categories: "
+            optionsList={categories}
+            value={selectedCategory.value}
+          />
+        )}
+      </div>
+
+      <div className="ecommerce__filter-panel-button">
+        <Button
+          handleClick={handleClickFilter}
+          color="primary"
+          label="Filter"
+          size="5px 45px"
+          type="button"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default FilterPanel
