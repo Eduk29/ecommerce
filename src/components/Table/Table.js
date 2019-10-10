67acc81796143b82c765f8ba87a1745979e07d81
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-10-10 00:00:10 -0300

    feat (WIP): Impl.  columns  in table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,22 +4,22 @@ import React from "react"
 // Styles
 import "../../style/components/Table.scss"
 
+const columns = ["Product", "Quantity", "Price", "Actions"]
+
 const Table = () => {
   return (
     <table className="ecommerce__table">
       <thead className="ecommerce__table-header">
         <tr className="ecommerce__table-header-row">
-          <th className="ecommerce__table-header-row-item">Product</th>
-          <th className="ecommerce__table-header-row-item">Quantity</th>
-          <th className="ecommerce__table-header-row-item">Price</th>
-          <th className="ecommerce__table-header-row-item">Actions</th>
+          {columns.map(item => {
+            return <th className="ecommerce__table-header-row-item">{item}</th>
+          })}
         </tr>
       </thead>
       <tbody className="ecommerce__table-body">
         <tr className="ecommerce__table-body-row">
           <td className="ecommerce__table-body-row-item--first-column">
-            {" "}
-            Produto 1{" "}
+            Produto 1
           </td>
           <td className="ecommerce__table-body-row-item"> 22 </td>
           <td className="ecommerce__table-body-row-item"> R$ 23,00 </td>
@@ -27,8 +27,7 @@ const Table = () => {
         </tr>
         <tr className="ecommerce__table-body-row">
           <td className="ecommerce__table-body-row-item--first-column">
-            {" "}
-            Produto 2{" "}
+            Produto 2
           </td>
           <td className="ecommerce__table-body-row-item"> 3 </td>
           <td className="ecommerce__table-body-row-item"> R$ 233,00 </td>
