7ab3cd0fa480251324024ae2040abf117f579a9e
Eduardo Marques <edu.temarques@gmail.com>
2019-10-16 22:36:47 -0300

    feat (WIP): Add Customers Information component

---
+++ src/components/CustomersInformation/CustomersInformation.js
@@ -0,0 +1,13 @@
+import React from "react"
+import InputText from "../InputText/InputText"
+
+const CustomersInformation = () => {
+  return (
+    <div>
+      Customers Area
+      <InputText />
+    </div>
+  )
+}
+
+export default CustomersInformation
