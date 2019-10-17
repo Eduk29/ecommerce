40b668c0e7309d4d8fb6d1797d1e447ffe7d06a9
Eduardo Marques <edu.temarques@gmail.com>
2019-10-16 22:24:49 -0300

    feat: Add OrderCart container

---
+++ src/containers/orderCart/orderCart.js
@@ -0,0 +1,44 @@
+// React
+import React from "react"
+
+// Components
+import Table from "../../components/Table"
+
+// Styles
+import "../../style/containers/OrderCart.scss"
+
+const columns = ["Product", "Quantity", "Price", "Actions"]
+
+const OrderCart = props => {
+  const { cart } = props
+
+  const displayTable = () => {
+    return cart.products.length > 0
+  }
+
+  return (
+    <div className="ecommerce__order-cart-container">
+      <div className="ecommerce__order-cart-container-order-summary">
+        {displayTable() && (
+          <div className="ecommerce__order-cart-container-order-summary--filled-cart">
+            <h4 className="ecommerce__order-cart-container-order-summary--filled-cart-title">
+              {" "}
+              Your products in cart{" "}
+            </h4>
+            <Table columns={columns} products={cart.products} />
+          </div>
+        )}
+        {!displayTable() && (
+          <h4 className="ecommerce__order-cart-container-order-summary--empty-cart">
+            Não há produtos no carrinho
+          </h4>
+        )}
+      </div>
+      <div className="ecommerce__order-cart-container-customer-information">
+        Customers Area
+      </div>
+    </div>
+  )
+}
+
+export default OrderCart
