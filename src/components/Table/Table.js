fd17da3381adb4997e42eef714c51e0796ee557c
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-10-24 20:12:18 -0300

    feat: Adjusts in table layout

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,6 +1,10 @@
 // React
 import React from "react"
 
+// Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+
 // Styles
 import "../../style/components/Table.scss"
 
@@ -33,7 +37,13 @@ const Table = props => {
               <td className="ecommerce__table-body-row-item">
                 R$ {product.item.price}
               </td>
-              <td className="ecommerce__table-body-row-item"> </td>
+              <td className="ecommerce__table-body-row-item">
+                <FontAwesomeIcon
+                  className="ecommerce__navbar-link-button"
+                  icon={faTrash}
+                  size="lg"
+                />
+              </td>
             </tr>
           )
         })}
