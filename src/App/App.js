6eab2f4dd02c7c92e9a0ee248429a1519af90c1b
Eduardo Marques <edu.temarques@gmail.com>
2019-10-26 13:57:24 -0300

    feat: Add Checkout page

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom"
 // Pages
 import Home from "../pages/Home"
 import Cart from "../pages/Cart"
+import Checkout from "../pages/Checkout"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/cart" exact component={Cart} />
+        <Route path="/checkout" exact component={Checkout} />
       </Switch>
     </BrowserRouter>
   )
---
+++ src/pages/Checkout/Checkout.js
@@ -0,0 +1,8 @@
+// React
+import React from "react"
+
+const Checkout = () => {
+  return <div>Checkout Page</div>
+}
+
+export default Checkout
